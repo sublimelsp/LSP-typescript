d2615738c9dd2c2b13e0c5d11c330b68c3589cb6
Rafał Chłodnicki <rchl2k@gmail.com>
2020-05-03 12:33:51 +0200

    Re-add _typescript.rename handling

--- plugin.py
+++ plugin.py
@@ -1,4 +1,8 @@
 import os
+import sublime
+from LSP.plugin.core.protocol import Point
+from LSP.plugin.core.url import uri_to_filename
+from LSP.plugin.core.views import point_to_offset
 from lsp_utils import NpmClientHandler
 
 
@@ -16,3 +20,23 @@ class LspTypescriptPlugin(NpmClientHandler):
     server_binary_path = os.path.join(
         server_directory, 'node_modules', 'typescript-language-server', 'lib', 'cli.js'
     )
+
+    def on_ready(self, api) -> None:
+        api.on_request('_typescript.rename', self._on_typescript_rename)
+
+    def _on_typescript_rename(self, textDocumentPositionParams, respond):
+        view = sublime.active_window().open_file(
+            uri_to_filename(textDocumentPositionParams['textDocument']['uri'])
+        )
+
+        if view:
+            lsp_point = Point.from_lsp(textDocumentPositionParams['position'])
+            point = point_to_offset(lsp_point, view)
+
+            sel = view.sel()
+            sel.clear()
+            sel.add_all([point])
+            view.run_command('lsp_symbol_rename')
+
+        # Server doesn't require any specific response.
+        respond(None)
