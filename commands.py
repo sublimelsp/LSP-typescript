c68091522a74e540c446efb42fef9f047d390279
Rafal Chlodnicki <rchl2k@gmail.com>
2022-08-21 22:26:01 +0200

    updates

--- commands.py
+++ commands.py
@@ -1,9 +1,9 @@
 from .protocol import Call, CallsDirection, CallsRequestParams, CallsResponse
 from LSP.plugin import Request
 from LSP.plugin import Session
-from LSP.plugin.core.protocol import Error, ExecuteCommandParams, Location, LocationLink
+from LSP.plugin.core.protocol import Location, LocationLink
 from LSP.plugin.core.registry import LspTextCommand
-from LSP.plugin.core.typing import Any, List, Optional, Union
+from LSP.plugin.core.typing import List, Optional, Union
 from LSP.plugin.core.views import text_document_position_params
 from LSP.plugin.execute_command import LspExecuteCommand
 from LSP.plugin.locationpicker import LocationPicker
@@ -11,11 +11,30 @@ import functools
 import sublime
 
 
-SESSION_NAME = "LSP-typescript"
+SESSION_NAME = __package__
 
 
-class LspTypescriptOrganizeImportsCommand(LspExecuteCommand):
+class LspTypescriptExecuteCommand(LspExecuteCommand):
     session_name = SESSION_NAME
+
+
+class LspTypescriptGotoSourceDefinitionCommand(LspTypescriptExecuteCommand):
+    def handle_success_async(self, result: Union[List[Location], List[LocationLink]], command_name: str) -> None:
+        if not result:
+            window = self.view.window()
+            window and window.status_message('No source definitions found')
+            return
+        session = self.session_by_name()
+        if not session:
+            return
+        if len(result) == 1:
+            args = {
+                'location': result[0],
+                'session_name': self.session_name,
+            }
+            self.view.run_command('lsp_open_location', args)
+        else:
+            LocationPicker(self.view, session, result, side_by_side=False)
 
 
 class LspTypescriptCallsCommand(LspTextCommand):
@@ -48,25 +67,4 @@ class LspTypescriptCallsCommand(LspTextCommand):
             }
 
         locations = list(map(to_location_link, result['calls']))
-        self.view.run_command("add_jump_record", {"selection": [(r.a, r.b) for r in self.view.sel()]})
         LocationPicker(self.view, session, locations, side_by_side=False)
-
-
-class LspTypescriptGotoSourceDefinitionCommand(LspExecuteCommand):
-    session_name = SESSION_NAME
-    command_name = '_typescript.goToSourceDefinition'
-
-    def run(self, edit: sublime.Edit, command_args: List[Any], event: Optional[dict] = None) -> None:
-        session = self.session_by_name()
-        if session:
-            params = {
-                'arguments': self._expand_variables(command_args),
-                'command': self.command_name
-            }  # type: ExecuteCommandParams
-            session.execute_command(params, progress=True).then(functools.partial(self.on_result_async, session))
-
-    def on_result_async(self, session: Session, response: Union[List[Location], Error]) -> None:
-        if isinstance(response, Error):
-            sublime.message_dialog('command {} failed. Reason: {}'.format(self.command_name, str(response)))
-            return
-        LocationPicker(self.view, session, response, side_by_side=False)
