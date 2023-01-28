68cb8aca48c2cdb22e723d4dc0aa485f92867b84
Rafal Chlodnicki <rchl2k@gmail.com>
2023-01-28 14:35:47 +0100

    Show used TypeScript version in the status bar

--- plugin.py
+++ plugin.py
@@ -1,7 +1,8 @@
 from LSP.plugin import uri_to_filename
 from LSP.plugin.core.protocol import Point, TextDocumentPositionParams
-from LSP.plugin.core.typing import Callable, Tuple
+from LSP.plugin.core.typing import Callable, Dict, Tuple
 from LSP.plugin.core.views import point_to_offset
+from lsp_utils import notification_handler
 from lsp_utils import NpmClientHandler
 from lsp_utils import request_handler
 import os
@@ -26,20 +27,27 @@ class LspTypescriptPlugin(NpmClientHandler):
         return (14, 16, 0)
 
     @request_handler('_typescript.rename')
-    def on_typescript_rename(
-        self, position_params: TextDocumentPositionParams, respond: Callable[[None], None]
-    ) -> None:
-        filename = uri_to_filename(position_params['textDocument']['uri'])
+    def on_typescript_rename(self, params: TextDocumentPositionParams, respond: Callable[[None], None]) -> None:
+        filename = uri_to_filename(params['textDocument']['uri'])
         view = sublime.active_window().open_file(filename)
-
         if view:
-            lsp_point = Point.from_lsp(position_params['position'])
+            lsp_point = Point.from_lsp(params['position'])
             point = point_to_offset(lsp_point, view)
-
             sel = view.sel()
             sel.clear()
             sel.add_all([point])
             view.run_command('lsp_symbol_rename')
-
         # Server doesn't require any specific response.
         respond(None)
+
+    @notification_handler('$/typescriptVersion')
+    def on_typescript_version_async(self, params: Dict[str, str]) -> None:
+        session = self.weaksession()
+        if not session:
+            return
+        version_template = session.config.settings.get('settings.statusText')
+        if not version_template or not isinstance(version_template, str):
+            return
+        status_text = version_template.replace('$version', params['version']).replace('$source', params['source'])
+        if status_text:
+            session.set_config_status_async(status_text)
