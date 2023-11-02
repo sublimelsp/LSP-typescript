9b0541e00781155d65029484d3b98c16c628d7e2
Rafal Chlodnicki <rchl2k@gmail.com>
2023-11-02 09:10:29 +0100

    add support for code lens

--- plugin.py
+++ plugin.py
@@ -1,8 +1,10 @@
 from .plugin_types import TypescriptVersionNotificationParams
+from LSP.plugin import Session
 from LSP.plugin import uri_to_filename
-from LSP.plugin.core.protocol import Point, TextDocumentPositionParams
-from LSP.plugin.core.typing import Callable, Tuple
+from LSP.plugin.core.protocol import Location, Point, TextDocumentPositionParams
+from LSP.plugin.core.typing import Any, Callable, List, Mapping, Tuple
 from LSP.plugin.core.views import point_to_offset
+from LSP.plugin.locationpicker import LocationPicker
 from lsp_utils import notification_handler
 from lsp_utils import NpmClientHandler
 from lsp_utils import request_handler
@@ -52,3 +54,29 @@ class LspTypescriptPlugin(NpmClientHandler):
         status_text = version_template.replace('$version', params['version']).replace('$source', params['source'])
         if status_text:
             session.set_config_status_async(status_text)
+
+    def on_pre_server_command(self, command: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
+        command_name = command['command']
+        if command_name == 'editor.action.showReferences':
+            session = self.weaksession()
+            if not session:
+                return False
+            self._handle_show_references(session, command['arguments'][2])
+            done_callback()
+            return True
+        return False
+
+    def _handle_show_references(self, session: Session, references: List[Location]) -> None:
+        view = session.window.active_view()
+        if not view:
+            return
+        if len(references) == 1:
+            args = {
+                'location': references[0],
+                'session_name': session.config.name,
+            }
+            session.window.run_command('lsp_open_location', args)
+        elif references:
+            LocationPicker(view, session, references, side_by_side=False)
+        else:
+            sublime.status_message('No references found')
