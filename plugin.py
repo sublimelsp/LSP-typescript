9380b70e5e90d4606d09036ee6cf2751434f8ded
Rafal Chlodnicki <rchl2k@gmail.com>
2025-08-26 08:46:16 +0200

    support vue ls 3.x

--- plugin.py
+++ plugin.py
@@ -1,15 +1,24 @@
+from __future__ import annotations
+from .plugin_types import TypescriptPluginContribution
 from .plugin_types import TypescriptVersionNotificationParams
+from LSP.plugin import ClientConfig
 from LSP.plugin import Session
 from LSP.plugin import uri_to_filename
+from LSP.plugin import WorkspaceFolder
 from LSP.plugin.core.protocol import Location, Point, TextDocumentPositionParams
-from LSP.plugin.core.typing import Any, Callable, List, Mapping, Tuple
+from LSP.plugin.core.typing import cast, Any, Callable, List, Mapping, Tuple
 from LSP.plugin.core.views import point_to_offset
 from LSP.plugin.locationpicker import LocationPicker
 from lsp_utils import notification_handler
 from lsp_utils import NpmClientHandler
 from lsp_utils import request_handler
+from sublime_lib import ResourcePath
 import os
 import sublime
+
+
+def log(message: str) -> None:
+    print(f'[{__package__}] {message}')
 
 
 def plugin_loaded() -> None:
@@ -18,16 +27,67 @@ def plugin_loaded() -> None:
 
 def plugin_unloaded() -> None:
     LspTypescriptPlugin.cleanup()
+    LspTypescriptPlugin.typescript_plugins = None
+
+
+def find_typescript_plugin_contributions() -> list[TypescriptPluginContribution]:
+    variables = {'storage_path': LspTypescriptPlugin.storage_path()}
+    resources = ResourcePath.glob_resources('typescript-plugins.json')
+    plugins: list[TypescriptPluginContribution] = []
+    for resource in resources:
+        try:
+            contributed_plugins = sublime.decode_value(resource.read_text())
+        except Exception:
+            log(f'Failed parsing schema "{resource.file_path()}"')
+            continue
+        if not isinstance(contributed_plugins, list):
+            log(f'Invalid contents of schema "{resource.file_path()}"')
+            continue
+        contributed_plugins = cast('list[TypescriptPluginContribution]', contributed_plugins)
+        for plugin in contributed_plugins:
+            name = plugin['name']
+            location = cast('str', sublime.expand_variables(plugin['location'], variables))
+            fullpath = os.path.join(location, name)
+            if not os.path.exists(fullpath):
+                log(f'Ignoring non-existant plugin at "{fullpath}"')
+                continue
+            plugins.append({
+                'name': name,
+                'languages': plugin['languages'],
+                'location': location,
+                'selector': plugin['selector']
+            })
+    return plugins
 
 
 class LspTypescriptPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'typescript-language-server'
     server_binary_path = os.path.join(server_directory, 'node_modules', 'typescript-language-server', 'lib', 'cli.mjs')
+    typescript_plugins: list[TypescriptPluginContribution] | None = None
 
     @classmethod
     def minimum_node_version(cls) -> Tuple[int, int, int]:
         return (14, 16, 0)
+
+    @classmethod
+    def on_configuration_loaded(cls, window: sublime.Window, workspace_folders: list[WorkspaceFolder],
+                                configuration: ClientConfig) -> None:
+        cls._handle_typescript_plugins(configuration)
+
+    @classmethod
+    def _handle_typescript_plugins(cls, configuration: ClientConfig) -> None:
+        if cls.typescript_plugins is None:
+            cls.typescript_plugins = find_typescript_plugin_contributions()
+        plugins = configuration.init_options.get('plugins') or []
+        for plugin in cls.typescript_plugins:
+            plugins.append({
+                'name': plugin['name'],
+                'languages': plugin['languages'],
+                'location': plugin['location'],
+            })
+            configuration.selector += f', {plugin["selector"]}'
+        configuration.init_options.set('plugins', plugins)
 
     @request_handler('_typescript.rename')
     def on_typescript_rename(self, params: TextDocumentPositionParams, respond: Callable[[None], None]) -> None:
--- plugin_types.py
+++ plugin_types.py
@@ -1,7 +1,15 @@
-from LSP.plugin.core.typing import Literal, TypedDict, Union
+from __future__ import annotations
+from LSP.plugin.core.typing import List, Literal, TypedDict, Union
 
 
 TypescriptVersionNotificationParams = TypedDict('TypescriptVersionNotificationParams', {
     'version': str,
     'source': Union[Literal['bundled'], Literal['user-setting'], Literal['workspace']]
+})
+
+TypescriptPluginContribution = TypedDict('TypescriptPluginContribution', {
+    'name': str,
+    'languages': List[str],
+    'location': str,
+    'selector': str,
 })
