37c256511e6e993be7ba1abb73aeb07caeb94147
Rafał Chłodnicki <rchl2k@gmail.com>
2026-03-31 20:38:46 +0200

    Replace `lsp_utils` API decorators with ones from `LSP` (#294)

--- plugin.py
+++ plugin.py
@@ -1,29 +1,40 @@
 from __future__ import annotations
+
 from .plugin_types import ApplyRefactoringCommand
 from .plugin_types import MoveToFileQuickPanelItem
 from .plugin_types import MoveToFileQuickPanelItemId
-from .plugin_types import ShowReferencesCommand
+from .plugin_types import ShowReferencesArguments
 from .plugin_types import TypescriptPluginContribution
 from .plugin_types import TypescriptVersionNotificationParams
 from functools import partial
 from LSP.plugin import ClientConfig
+from LSP.plugin import notification_handler
 from LSP.plugin import parse_uri
+from LSP.plugin import Promise
+from LSP.plugin import request_handler
+from LSP.plugin import uri_from_view
 from LSP.plugin import WorkspaceFolder
-from LSP.plugin.core.protocol import Error, Point
+from LSP.plugin.core.protocol import Error
+from LSP.plugin.core.protocol import Point
 from LSP.plugin.core.views import point_to_offset
 from LSP.plugin.locationpicker import LocationPicker
-from lsp_utils import notification_handler
+from LSP.protocol import Location
 from lsp_utils import NpmClientHandler
-from lsp_utils import request_handler
 from pathlib import Path
 from sublime_lib import ResourcePath
-from typing import TYPE_CHECKING, Any, cast, Callable
+from typing import Any
+from typing import Callable
+from typing import cast
+from typing import final
+from typing import TYPE_CHECKING
 from typing_extensions import override
 import os
 import sublime
 
 if TYPE_CHECKING:
-    from LSP.protocol import ConfigurationItem, ExecuteCommandParams, TextDocumentPositionParams
+    from LSP.protocol import ConfigurationItem
+    from LSP.protocol import ExecuteCommandParams
+    from LSP.protocol import TextDocumentPositionParams
 
 
 MOVE_TO_FILE_QUICK_PANEL_ITEMS: list[MoveToFileQuickPanelItem] = [
@@ -52,7 +63,7 @@ def find_typescript_plugin_contributions() -> list[TypescriptPluginContribution]
     for resource in resources:
         try:
             contributed_plugins = sublime.decode_value(resource.read_text())
-        except Exception:
+        except ValueError:
             log(f'Failed parsing schema "{resource.file_path()}"')
             continue
         if not isinstance(contributed_plugins, list):
@@ -78,29 +89,35 @@ def find_typescript_plugin_contributions() -> list[TypescriptPluginContribution]
     return plugins
 
 
+@final
 class LspTypescriptPlugin(NpmClientHandler):
-    package_name = __package__
+    package_name = str(__package__)
     server_directory = 'typescript-language-server'
-    server_binary_path = Path(server_directory) / 'node_modules' / 'typescript-language-server' / 'lib' / 'cli.mjs'
+    server_binary_path = str(Path(server_directory) / 'node_modules' / 'typescript-language-server' / 'lib' / 'cli.mjs')
     typescript_plugins: list[TypescriptPluginContribution] | None = None
 
     @classmethod
+    @override
     def minimum_node_version(cls) -> tuple[int, int, int]:
         return (20, 0, 0)
 
     @classmethod
-    def selector(cls, view: sublime.View, config: ClientConfig) -> str:
-        plugins = cls._get_typescript_plugins()
-        new_selector = config.selector
-        for plugin in plugins:
-            if "selector" in plugin:
-                new_selector += f', {plugin["selector"]}'
-        return new_selector
+    @override
+    def is_applicable(cls, view: sublime.View, config: ClientConfig) -> bool:
+        if super().is_applicable(view, config):
+            return True
+        scheme, _ = parse_uri(uri_from_view(view))
+        if scheme in config.schemes and (syntax := view.syntax()):
+            for plugin in cls._get_typescript_plugins():
+                if (selector := plugin.get('selector')) and sublime.score_selector(syntax.scope, selector) > 0:
+                    return True
+        return False
 
     @classmethod
+    @override
     def on_pre_start(cls, window: sublime.Window, initiating_view: sublime.View,
                      workspace_folders: list[WorkspaceFolder], configuration: ClientConfig) -> str | None:
-        plugins = configuration.init_options.get('plugins') or []
+        plugins = configuration.initialization_options.get('plugins') or []
         for ts_plugin in cls._get_typescript_plugins():
             plugin: TypescriptPluginContribution = {
                 'name': ts_plugin['name'],
@@ -109,7 +126,7 @@ class LspTypescriptPlugin(NpmClientHandler):
             if 'languages' in ts_plugin:
                 plugin['languages'] = ts_plugin['languages']
             plugins.append(plugin)
-        configuration.init_options.set('plugins', plugins)
+        configuration.initialization_options.set('plugins', plugins)
         return None
 
     @classmethod
@@ -119,7 +136,7 @@ class LspTypescriptPlugin(NpmClientHandler):
         return cls.typescript_plugins
 
     @request_handler('_typescript.rename')
-    def on_typescript_rename(self, params: TextDocumentPositionParams, respond: Callable[[None], None]) -> None:
+    def on_typescript_rename(self, params: TextDocumentPositionParams) -> Promise[None]:
         _, filename = parse_uri(params['textDocument']['uri'])
         view = sublime.active_window().open_file(filename)
         if view:
@@ -130,7 +147,7 @@ class LspTypescriptPlugin(NpmClientHandler):
             sel.add_all([point])
             view.run_command('lsp_symbol_rename')
         # Server doesn't require any specific response.
-        respond(None)
+        return Promise.resolve(None)
 
     @notification_handler('$/typescriptVersion')
     def on_typescript_version_async(self, params: TypescriptVersionNotificationParams) -> None:
@@ -158,30 +175,32 @@ class LspTypescriptPlugin(NpmClientHandler):
             }
         return configuration
 
+    @override
     def on_pre_server_command(self, command: ExecuteCommandParams, done_callback: Callable[[], None]) -> bool:
         command_name = command['command']
+        if 'arguments' not in command:
+            return False
         if command_name == 'editor.action.showReferences':
-            references_command = cast('ShowReferencesCommand', command)
-            self._handle_show_references(references_command)
+            _, __, references = cast('ShowReferencesArguments', cast('object', command['arguments']))
+            self._handle_show_references(references)
             done_callback()
             return True
         if command_name == '_typescript.applyRefactoring':
-            refactor_command = cast('ApplyRefactoringCommand', command)
-            if self._handle_apply_refactoring(refactor_command):
+            refactoring_command = cast('ApplyRefactoringCommand', cast('object', command))
+            if self._handle_apply_refactoring(refactoring_command):
                 done_callback()
                 return True
         return False
 
-    def _handle_show_references(self, references_command: ShowReferencesCommand) -> None:
+    def _handle_show_references(self, references: list[Location]) -> None:
         session = self.weaksession()
         if not session:
             return
         view = session.window.active_view()
         if not view:
             return
-        references = references_command['arguments'][2]
         if len(references) == 1:
-            args = {
+            args: dict[str, Any] = {
                 'location': references[0],
                 'session_name': session.config.name,
             }
@@ -192,8 +211,7 @@ class LspTypescriptPlugin(NpmClientHandler):
             sublime.status_message('No references found')
 
     def _handle_apply_refactoring(self, command: ApplyRefactoringCommand) -> bool:
-        argument = command['arguments'][0]
-        if argument['action'] == 'Move to file':
+        if command['arguments'][0]['action'] == 'Move to file':
             return self._handle_move_to_file(command)
         return False
 
